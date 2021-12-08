18c7faf0230c9b96b40d847ca9d274cbc1399464
huangbc <huangbc_6603@163.com>
2021-12-08 10:47:32 +0800

    wxpay_demo提交github...Test

---
+++ src/main/java/com/github/wxpay/sdk/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.github.wxpay.sdk;
+
+/**
+ * @author huangbc
+ * @version 1.0
+ * @date 2021/12/8 10:26
+ */
+public class HelloWorld {
+}
---
+++ src/main/java/com/github/wxpay/sdk/Test.java
@@ -0,0 +1,28 @@
+package com.github.wxpay.sdk;
+
+import java.math.BigDecimal;
+
+/**
+ * @author huangbc
+ * @version 1.0
+ * @date 2021/12/7 18:54
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println(0.3 - 0.1);
+        System.out.println(0.2 + 0.1);
+        System.out.println(0.2 * 0.1);
+        System.out.println(0.3 / 0.1);
+        System.out.println(new BigDecimal("0.3").multiply(new BigDecimal("0.1")));//乘
+        System.out.println(new BigDecimal("0.3").max(new BigDecimal("0.1")));//
+        System.out.println(new BigDecimal("0.3").min(new BigDecimal("0.1")));
+        System.out.println(new BigDecimal("0.3").add(new BigDecimal("0.1")));//加
+        System.out.println(new BigDecimal("0.3").divide(new BigDecimal("0.1")));//除
+        System.out.println(new BigDecimal("0.3").subtract(new BigDecimal(10)));
+
+
+    }
+
+}
