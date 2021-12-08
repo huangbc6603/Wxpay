d30e5d2a0ec53b87df24fd5bf111ced4d2dcf655
huangbc <huangbc_6603@163.com>
2021-12-08 12:23:20 +0800

    加减乘除

--- src/main/java/com/github/wxpay/sdk/Test.java
+++ src/main/java/com/github/wxpay/sdk/Test.java
@@ -15,11 +15,11 @@ public class Test {
         System.out.println(0.2 + 0.1);
         System.out.println(0.2 * 0.1);
         System.out.println(0.3 / 0.1);
-        System.out.println(new BigDecimal("0.3").multiply(new BigDecimal("0.1")));//乘
+        System.out.println(new BigDecimal("0.3").multiply(new BigDecimal("0.1")));//乘法
         System.out.println(new BigDecimal("0.3").max(new BigDecimal("0.1")));//
         System.out.println(new BigDecimal("0.3").min(new BigDecimal("0.1")));
-        System.out.println(new BigDecimal("0.3").add(new BigDecimal("0.1")));//加
-        System.out.println(new BigDecimal("0.3").divide(new BigDecimal("0.1")));//除
+        System.out.println(new BigDecimal("0.3").add(new BigDecimal("0.1")));//加法
+        System.out.println(new BigDecimal("0.3").divide(new BigDecimal("0.1")));//除法
         System.out.println(new BigDecimal("0.3").subtract(new BigDecimal(10)));//减法。。
 
 
