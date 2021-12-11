c09593e12a654b37ed60a15f833ae24ba543f5f6
huangbc <huangbc_6603@163.com>
2021-12-11 13:29:30 +0800

    遍历map

--- src/main/java/com/github/wxpay/sdk/HelloWorld.java
+++ src/main/java/com/github/wxpay/sdk/HelloWorld.java
@@ -1,9 +1,44 @@
 package com.github.wxpay.sdk;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @author huangbc
  * @version 1.0
  * @date 2021/12/8 10:26
  */
 public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        Map<String, String> hashMap = new HashMap();
+        hashMap.put("huang", "bc");
+        hashMap.put("zhang", "sanfeng");
+        Set<Map.Entry<String, String>> entries = hashMap.entrySet();
+        for (Map.Entry<String, String> entry : entries) {
+            System.out.println(entry.getKey() +":"+ entry.getValue());
+        }
+
+        Set<String> keySet = hashMap.keySet();
+        for (String s : keySet) {
+            System.out.println(s);
+        }
+
+        for (String value : hashMap.values()) {
+            System.out.println(value);
+        }
+
+        Iterator<Map.Entry<String, String>> iterator = entries.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> next = iterator.next();
+            next.getKey();
+            next.getValue();
+        }
+
+    }
+
+
 }
