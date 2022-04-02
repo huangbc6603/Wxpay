cdc757cc4cc7c92372386d2a5c20c798236cff23
Derek-huang <huangbc_6603@163.com>
2022-04-02 11:30:18 +0800

    add tests

---
+++ src/main/java/com/github/wxpay/sdk/utils/Test3.java
@@ -0,0 +1,11 @@
+package com.github.wxpay.sdk.utils;
+
+/**
+ * @author Derek-huang
+ */
+public class Test3 {
+
+  public static void main(String[] args) {
+
+  }
+}
---
+++ src/main/java/com/github/wxpay/sdk/utils/Test4.java
@@ -0,0 +1,11 @@
+package com.github.wxpay.sdk.utils;
+
+/**
+ * @author Derek-huang
+ */
+public class Test4 {
+
+  public static void main(String[] args) {
+
+  }
+}
